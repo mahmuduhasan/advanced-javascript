813e6e3f63487f5eb94b9d1cd94c63a4f7779668
mahmuduhasan <codermodule@gmail.com>
2020-03-05 22:33:04 +0600

    Null vs Undefined done

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+//Undefined
+
+// let number;
+// console.log(number);
+
+// function addIt(num1 , num2){
+//     const add = num1 + num2;
+// }
+// const result = addIt(2,3);
+// console.log(result);
+
+// function addIt(num1 , num2){
+//     const add = num1 + num2;
+//     return
+// }
+// const result = addIt(2,3);
+// console.log(result);
+
+// function add( num1,num2){
+//     console.log(num1 , num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// const premik = {name : "Smart Dude", phone:012454 };
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+// let age =[4,6,5];
+// console.log(age[10]);
+
+//Main difference between Null and Undefined is Null declared if there is no need of value but if there is no declaration or there is no such value which is trying to access by anyone is Undefined.
