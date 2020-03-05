c0ae321a8eeac60645a5f9e64063ed42fe5a5920
mahmuduhasan <codermodule@gmail.com>
2020-03-06 02:22:05 +0600

    done module 15

---
+++ slice-splice-join.js
@@ -0,0 +1,13 @@
+const number =[1,2,3,4,5,6,7];
+
+const take = number.slice(1,6);
+console.log(take);
+
+const together = number.join(",");
+console.log(together);
+
+const takeHome = number.splice(2,4);
+console.log(takeHome);
+
+const getTogether = number.join(",");
+console.log(getTogether);
